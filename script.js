989a04cb4dbddbe5f805f04207de7e70e0a168b1
RAPHA <rapha.alves74@gmail.com>
2024-10-01 16:13:04 -0300

    primeiro js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Definindo a função
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função três vezes
+for (let i = 0; i < 5; i++) {
+    imprimirMensagem();
+}
